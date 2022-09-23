527f3e2d0a1fc72e81bfc1207f03f728a5077c1d
Dan L <DJL.Consulting@outlook.com>
2022-09-23 22:19:08 +0400

    Add additional depenedencies to API project.

--- VS/DataCommandCenter/DataCommandCenter.API/Program.cs
+++ VS/DataCommandCenter/DataCommandCenter.API/Program.cs
@@ -2,15 +2,62 @@ using DataCommandCenter.DAL;
 using DataCommandCenter.DAL.Models;
 using DataCommandCenter.DAL.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Reflection;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/DCC_Log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog();
+
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson()
+.AddXmlDataContractSerializerFormatters();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+
+    //setupAction.AddSecurityDefinition("CityInfoApiBearerAuth", new OpenApiSecurityScheme()
+    //{
+    //    Type = SecuritySchemeType.Http,
+    //    Scheme = "Bearer",
+    //    Description = "Input a valid token to access this API"
+    //});
+
+    //setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+    //{
+    //    {
+    //        new OpenApiSecurityScheme
+    //        {
+    //            Reference = new OpenApiReference {
+    //                Type = ReferenceType.SecurityScheme,
+    //                Id = "CityInfoApiBearerAuth" }
+    //        }, new List<string>() }
+    //});
+
+});
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -20,6 +67,13 @@ builder.Services.AddDbContext<DataCommandCenterContext>(
         builder.Configuration["ConnectionStrings:DCCConnectionString"]));
 
 builder.Services.AddScoped<IDCCRepository, DCCRepository>();
+
+builder.Services.AddApiVersioning(setupAction =>
+{
+    setupAction.AssumeDefaultVersionWhenUnspecified = true;
+    setupAction.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    setupAction.ReportApiVersions = true;
+});
 
 var app = builder.Build();
 
@@ -32,8 +86,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
+app.UseEndpoints(endpoints => 
+{ 
+    endpoints.MapControllers(); 
+});
 
 app.Run();
